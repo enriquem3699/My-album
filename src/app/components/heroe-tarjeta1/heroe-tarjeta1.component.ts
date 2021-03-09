928b1f7fc5c21042ab234cf5c063585a28a69183
Enrique <1625939@utp.edu.pe>
2021-03-08 22:16:05 -0500

    first commit

---
+++ src/app/components/heroe-tarjeta1/heroe-tarjeta1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-heroe-tarjeta1',
+  templateUrl: './heroe-tarjeta1.component.html'
+})
+export class HeroeTarjeta1Component implements OnInit {
+
+ heroe:any={};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
